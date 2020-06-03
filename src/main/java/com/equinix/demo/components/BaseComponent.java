bd4fcf77105f271f9ededfcecac6bb72c8c175d1
Artur Owczarek <Artur.Owczarek@eu.equinix.com>
2020-06-03 19:51:38 +0200

    fixes

---
+++ src/main/java/com/equinix/demo/components/BaseComponent.java
@@ -0,0 +1,15 @@
+package com.equinix.demo.components;
+
+import com.equinix.demo.AbstractComponent;
+import org.openqa.selenium.By;
+
+public class BaseComponent extends AbstractComponent {
+
+    public BaseComponent(By elementLocator) {
+        super(elementLocator);
+    }
+
+    public boolean isEnabled() {
+        return findElement(elementLocator).isEnabled();
+    }
+}
--- src/test/java/com/equinix/demo/ExampleTest.java
+++ src/test/java/com/equinix/demo/ExampleTest.java
@@ -1,3 +1,5 @@
+package com.equinix.demo;
+
 import org.junit.Test;
 
 public class ExampleTest {
